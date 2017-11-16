38fd1a4c56a797082db85c186a11c2ec12adb330
Tatsuya Katsushima <t.katsushima@outlook.jp>
2017-11-16 23:20:53 +0900

    update templates

---
+++ template/InputReader.scala
@@ -0,0 +1,21 @@
+import java.io._
+import java.util.StringTokenizer
+
+class InputReader(stream: InputStream) {
+  private val reader = new BufferedReader(new InputStreamReader(stream))
+  private var tokenizer: StringTokenizer = new StringTokenizer(reader.readLine())
+  def next(): String = {
+    while (!tokenizer.hasMoreTokens()) {
+      tokenizer = new StringTokenizer(reader.readLine())
+    }
+    tokenizer.nextToken()
+  }
+}
+
+object Main extends App {
+  val in = new InputReader(System.in)
+ 
+  val n = in.next().toInt
+  val a, b = in.next().toInt
+  val s = in.next()
+}
