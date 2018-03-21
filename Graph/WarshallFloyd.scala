9ab1fb5a03391a01f2a5a306c9d4d2b51b1d225a
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2018-03-21 18:04:56 +0900

    WarshallFloyd の上限値をLong Maxに

--- Graph/WarshallFloyd.scala
+++ Graph/WarshallFloyd.scala
@@ -1,15 +1,16 @@
 // O(V^3)
-// V は頂点数
+// v は頂点数
 
 // 他ノードを経由しているかの確認
 // (i != x && i != y) && d(x)(y) == d(x)(i) + d(i)(y)
 
 
-val d = Array.ofDim[Int](v, v)  // d[a][b] は辺 e = (a, b) のコスト(存在しない場合はINF, ただし d[i][i] = 0 とする)
-val MAX = 1e9.toInt
+// d[a][b] は辺 e = (a, b) のコスト(存在しない場合はINF, ただし d[i][i] = 0 とする)
+val d = Array.ofDim[Long](v+1, v+1)
 
 // 初期化
-for(i <- 0 until v; j <- 0 until v){
+val MAX = Long.MaxValue
+for(i <- 1 to v; j <- 1 to v){
   if(i == j)
     d(i)(j) = 0
   else
@@ -18,8 +19,8 @@ for(i <- 0 until v; j <- 0 until v){
 
 // ここで d の入力行う
 
-// function
-def warshall_floyd() =
-  for(k <- 0 until v; i <- 0 until v; j <- 0 until v){
-    d(i)(j) = Math.min(d(i)(j), d(i)(k) + d(k)(j))
-  }
+
+// warshall_floyd
+for(k <- 1 to v; i <- 1 to v; j <- 1 to v){
+  d(i)(j) = Math.min(d(i)(j), d(i)(k) + d(k)(j))
+}
