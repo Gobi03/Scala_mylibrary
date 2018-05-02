6f778440a5c4c0bc76d02b64f9f06224f978021e
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2018-05-02 21:31:32 +0900

    Dijkstraの改修

--- Graph/Dijkstra.scala
+++ Graph/Dijkstra.scala
@@ -1,48 +1,49 @@
 // 要テスト
 
 // O(E log(V))
+import scala.util.control.Breaks.{breakable,break}
 import scala.collection.mutable.PriorityQueue
 
-val edge = new Array[List[Int]](v)  // 頂点 i から行くことができる頂点のリスト
-val dist = Array.ofDim[Int](a, b)  // 頂点 a から頂点 b に移動するコスト
+
+val edge = new Array[List[(Int, Int)]](v)  // (node, cost)
+
 
 // fro から to への最短距離(v は頂点数)
 def dijkstra(from: Int, to: Int, v: Int): Int = {
-  val MAX = 1e9.toInt
+  val MAX = Int.MaxValue
 
-  val d = Array.fill(v)(MAX)   // fro からの距離を格納する
+  val dist = Array.fill(v)(MAX)   // fro からの距離を格納する
   val check = new Array[Boolean](v)  // すでにその頂点からを調べたかのフラグ
 
-  d(from) = 0
-  // priority queue: (cost, node) queue
-  val pq = new PriorityQueue[(Int, Int)]
-  pq.enqueue((0, from))
+  dist(from) = 0
+
+  val pq = new PriorityQueue[(Int, Int)]  // (-cost, node) queue
+  pq.enqueue((-0, from))
 
   // 頂点は v 個あるので、v 周する
-  for(i <- 0 until v) {
+  for(_ <- 0 until v) {
     // 調べ済みでない頂点のうち、最も近い頂点を now に入れる
-    var now = -1       // このターンにフォーカスするノード
-    var nowdis = MAX   // その距離
+    var nowNode = -1       // このターンにフォーカスするノード
 
-    var flag = true
-    while(flag){
-      now = pq.dequeue._2  // 最小コストから貪欲に取っていく
-      if(!check(now)){  // まだチェックされていないノードだったら決定
-        check(now) = true
-        nowdis = d(now)
-        flag = false
+    breakable {
+      while(true) {
+        nowNode = pq.dequeue._2  // 最小コストから貪欲に取っていく
+        if(!check(nowNode)){  // まだチェックされていないノードだったら決定
+          check(nowNode) = true
+          break
+        }
       }
     }
 
     // その頂点からたどり着ける頂点の情報を更新する
-    for(next <- edge(now)){  // nex はたどり着ける頂点
-      val nextdis = d(now) + dist(now)(next)
-      if(nextdis < d(next)){  // 既存の通路より近いなら更新
-        d(next) = nextdis
-        pq.enqueue((d(next), next))
+    for((nextNode, cost) <- edge(nowNode)) {
+      val nextDist = dist(nowNode) + cost
+      if(nextDist < dist(nextNode)){  // 既存の通路より近いなら更新
+        dist(nextNode) = nextDist
+        pq.enqueue((-nextDist, nextNode))
       }
     }
   }
 
-  return d(to)
+  dist(to)
 }
