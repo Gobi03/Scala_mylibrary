bc24c87e0e33a59c439a4f3af6f02f599dc2335e
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2019-02-19 22:50:11 +0900

    ２進数の変換方法を追記

--- 基本操作/ビット演算.scala
+++ 基本操作/ビット演算.scala
@@ -2,7 +2,9 @@
 
 |  // 4 | 1 => 5
 &  // 3 & 1 => 1
-
+^ // XOR
+<< // 左シフト
+1 << n // 2のn乗
 
 /* n進数変換 */
 // ２進数
@@ -13,3 +15,6 @@
 254.toHexString
 
 // http://tnoda-scala.tumblr.com/post/84033322446/2-%E9%80%B2%E6%95%B0%E3%82%92%E4%BE%BF%E5%88%A9%E3%81%AB%E6%89%B1%E3%81%86%E3%81%9F%E3%82%81%E3%81%AB-java-%E3%81%AE%E5%8A%9B%E3%82%92%E5%80%9F%E3%82%8A%E3%82%8B
+
+import java.lang.Integer.parseInt
+parseInt("101", 2) // => 5
