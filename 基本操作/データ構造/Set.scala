f44173ab12a5a011120113d779a8aa71db7f007f
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2019-04-21 15:03:33 +0900

    modify Set example

--- 基本操作/データ構造/Set.scala
+++ 基本操作/データ構造/Set.scala
@@ -1,10 +1,11 @@
 import scala.collection.mutable.Set
 
 scala> val set = Set(2, 7, 1, 8, 2, 8)
-set: scala.collection.immutable.Set[Int] = Set(2, 7, 1, 8)
+set: scala.collection.mutable.Set[Int] = Set(2, 7, 1, 8)
 
 
 set1 += 42   // 要素の追加
+set1(42) // 要素の存在確認
 
 set1.intersect(set2)  // set1とset2の積集合求める
 set1.union(set2)    // set1とset2の和集合求める
