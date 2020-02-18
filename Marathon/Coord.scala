906928856d6a4eebe9822d5a7c14986e032e8250
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2020-02-18 16:37:42 +0900

    update Coord

--- Marathon/Coord.scala
+++ Marathon/Coord.scala
@@ -14,9 +14,11 @@ case class Coord(x: Int, y: Int) extends Ordered[Coord] {
   def +(co: Coord): Coord = {
     Coord(this.x + co.x, this.y + co.y)
   }
-
   def -(co: Coord): Coord = {
     Coord(this.x - co.x, this.y - co.y)
+  }
+  def *(n: Int): Coord = {
+    Coord(this.x * n, this.y * n)
   }
 
   def toPair: (Int, Int) = (x, y)
@@ -41,6 +43,27 @@ case class Coord(x: Int, y: Int) extends Ordered[Coord] {
     delta
       .map(_ + this)
       .filter(_.inField)
+  }
+
+  def dist(that: Coord): Int = {
+    (this.x - that.x).abs + (this.y - that.y).abs
+  }
+
+  def get2dim[A](ar: Array[Array[A]]): A = {
+    ar(this.y)(this.x)
+  }
+
+  // this から見て that がどの方向にあるか見る
+  def direct(that: Coord): Seq[String] = {
+    var res = Seq[String]()
+
+    if (this.x < that.x)      res = res :+ "R"
+    else if (this.x > that.x) res = res :+ "L"
+
+    if (this.y < that.y)      res = res :+ "D"
+    else if (this.y > that.y) res = res :+ "U"
+
+    res
   }
 
 }
