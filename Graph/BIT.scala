6443a29abadb0c8346d3e8838fb1b3ae9f760ac3
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2020-03-20 19:14:45 +0900

    BIT のリファクタ

--- Graph/BIT.scala
+++ Graph/BIT.scala
@@ -8,10 +8,8 @@ class BIT(n: Int) {
   // sum of [1, i]
   def sum(i: Int): Int = {
     def func(i: Int, res: Int): Int = {
-      if(i <= 0)
-        res
-      else
-        func(i - (i & -i), res + ar(i))
+      if(i <= 0) res
+      else func(i - (i & -i), res + ar(i))
     }
     func(i, 0)
   }
@@ -19,9 +17,7 @@ class BIT(n: Int) {
   // add x to i-th element
   def add(i: Int, x: Int): Unit = {
     def func(i: Int): Unit = {
-      if(i > n)
-        ()
-      else {
+      if(i <= n) {
         ar(i) += x
         func(i + (i & -i))
       }
