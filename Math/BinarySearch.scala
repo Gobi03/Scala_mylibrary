f17fc222db354f1739fec6b9db2ae3fc24f36574
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2020-03-20 19:15:40 +0900

    二分探索のユーザー更新部分が独立するように

--- Math/BinarySearch.scala
+++ Math/BinarySearch.scala
@@ -1,33 +1,45 @@
 // 条件を満たす最大の値を返す
-def binarySearch(min: Int, max: Int): Int = {
-  val mid: Int = (max + min) / 2
-  (max - min) match{
-    case 0 | 1 =>
-      check(max) match{
-        case true  => max
-        case false => min
-      }
-    case _ =>
-      (check(mid)) match{
-        case true  => binarySearch(mid, max)
-        case false => binarySearch(min, mid)
-      }
+def binarySearch(_min: Int, _max: Int): Int = {
+  def check(i: Int): Boolean = ???
+
+  def func(min: Int, max: Int): Int = {
+    val mid: Int = (max + min) / 2
+      (max - min) match{
+      case 0 | 1 =>
+        check(max) match{
+          case true  => max
+          case false => min
+        }
+      case _ =>
+        (check(mid)) match{
+          case true  => func(mid, max)
+          case false => func(min, mid)
+        }
+    }
   }
+
+  func(_min, _max)
 }
 
 // 条件を満たす最小の値を返す
-def binarySearch(min: Int, max: Int): Int = {
-  val mid: Int = (max + min) / 2
-  (max - min) match{
-    case 0 | 1 =>
-      check(min) match{
-        case true  => min
-        case false => max
-      }
-    case _ =>
-      (check(mid)) match{
-        case true  => binarySearch(min, mid)
-        case false => binarySearch(mid, max)
-      }
+def binarySearch(_min: Int, _max: Int): Int = {
+  def check(i: Int): Boolean = ???
+
+  def func(min: Int, max: Int): Int = {
+    val mid: Int = (max + min) / 2
+      (max - min) match{
+      case 0 | 1 =>
+        check(min) match{
+          case true  => min
+          case false => max
+        }
+      case _ =>
+        (check(mid)) match{
+          case true  => func(min, mid)
+          case false => func(mid, max)
+        }
+    }
   }
+
+  func(_min, _max)
 }
